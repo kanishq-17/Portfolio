e62cc5a03db4eaeaad17250aa31db5d247a213b7
Kanishq Sodhani <virusbaba017@gmail.com>
2025-11-18 22:51:57 +0530

    start freelance contact page ⌛

---
+++ src/router/LetsCollaborate.jsx
@@ -0,0 +1,12 @@
+const LetsCollaborate = () => {
+  return (
+    <div
+      className="w-full h-screen bg-custom-gradient"
+      style={{
+        backgroundImage: `linear-gradient(70deg, hsl(330deg 91% 9%) 0%, hsl(341deg 91% 18%) 39%, hsl(353deg 81% 26%) 69%, hsl(347deg 83% 30%) 85%, hsl(333deg 84% 34%) 94%, hsl(2deg 18% 63%) 100%)`,
+      }}
+    ></div>
+  );
+};
+
+export default LetsCollaborate;
