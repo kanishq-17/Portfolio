a9b22cfbce60907663655ec5537cb031474e7285
Kanishq Sodhani <virusbaba017@gmail.com>
2025-11-22 11:01:24 +0530

    setup for React bits

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-export const theme = {
-  extend: {
-    backgroundImage: {
-      "custom-gradient":
-        "linear-gradient(70deg, hsl(330deg 91% 9%) 0%, hsl(341deg 91% 18%) 39%, hsl(353deg 81% 26%) 69%, hsl(347deg 83% 30%) 85%, hsl(333deg 84% 34%) 94%, hsl(2deg 18% 63%) 100%)",
-    },
-  },
-};
+// export const theme = {
+//   extend: {
+//     backgroundImage: {
+//       "custom-gradient":
+//         "linear-gradient(50deg,    hsl(0deg 85% 97%) 0%,hsl(359deg 93% 96%) 0%, hsl(358deg 95% 94%) 0%, hsl(356deg 96% 93%) 0%,    hsl(355deg 95% 91%) 1%,    hsl(354deg 93% 89%) 2%,    hsl(352deg 92% 87%) 3%,    hsl(351deg 90% 86%) 5%,    hsl(349deg 88% 84%) 8%,    hsl(351deg 88% 85%) 10%,    hsl(352deg 87% 85%) 13%,    hsl(353deg 87% 86%) 17%,    hsl(355deg 86% 87%) 21%,    hsl(356deg 84% 88%) 26%,    hsl(357deg 82% 88%) 31%,    hsl(359deg 80% 89%) 37%,    hsl(0deg 77% 90%) 43%,    hsl(357deg 83% 90%) 50%,    hsl(354deg 88% 90%) 57%,    hsl(351deg 93% 89%) 64%,    hsl(349deg 96% 89%) 72%,    hsl(346deg 99% 89%) 80%,    hsl(342deg 100% 89%) 87%,    hsl(339deg 100% 88%) 94%,    hsl(336deg 100% 88%) 100%  )",
+//     },
+//   },
+// };
